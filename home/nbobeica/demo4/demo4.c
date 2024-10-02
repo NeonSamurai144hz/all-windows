7dcafd19656e09a7e405fef4518caca9382affa4
root <root@DESKTOP-44P6421>
2024-10-02 20:17:37 +0200

    demo4

---
+++ home/nbobeica/demo4/demo4.c
@@ -0,0 +1,20 @@
+  #include <stdlib.h>
+  #include <stdio.h>
+
+int main() {
+	printf("Entrer un mot: \n")
+	char mot [100];
+	int longeur = 0
+
+	fgets(word, sizeof(word), stdin);
+
+	while(word[length] != '\0') {
+	  if(word[length] == '\n') {
+	    break;
+
+		}
+	length++;
+	}
+
+	printf("La longeur du mot est %d\n", longeur )
+
