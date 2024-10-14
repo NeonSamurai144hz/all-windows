da08c15406c7f880ef0cc2221bdc3e0d2cd15e33
root <root@DESKTOP-44P6421>
2024-10-14 09:25:13 +0200

    add tic-tac-toe.h

---
+++ home/nbobeica/projet-tic-tac-toe/tic-tac-toe.h
@@ -0,0 +1,12 @@
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+
+void afficher_plateau(char *plateau);
+int verifier_victoire(char *plateau);
+int plateau_plein(char *plateau);
+void jouer_joueur(char *plateau);
+void jouer_ordinateur(char *plateau);
+
+#endif
+
